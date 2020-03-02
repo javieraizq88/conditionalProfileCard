238bfa1fac1deff0b09245de7d343f110b966785
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-03-02 14:49:44 -0300

    actualizado h2

--- src/js/index.js
+++ src/js/index.js
@@ -10,26 +10,35 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
   if (variables.name == null) variables.name = "Lucy";
-  if (variables.lastname == null) variables.lastname = 'Boilett';
-  if (variables.socialMediaPosition == "left") variables.socialMediaPosition-left;
+  if (variables.lastname == null) variables.lastname = "Boilett";
+  if (variables.socialMediaPosition == "position-left")
+    if (variables.twitter == null) variables.twiter = "";
+  if (variables.github == null) variables.github = "";
+  if (variables.linkedin == null) variables.linkedin = "";
+  if (variables.instagram == null) variables.instagram = "";
 
-  if (variables.twitter == null) variables.twitter = ;
-  if (variables.github == null) variables.github = ;
-  if (variables.linkedin == null) ;
-  if (variables.instagram == null) ;
+  if (variables.role == null) variables.role = "";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
+          <img src="${variables.avatarURL}" class="photo"/>
           <h1>${variables.name} ${variables.lastname}</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
-          <ul class="position-${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin
+            }"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
